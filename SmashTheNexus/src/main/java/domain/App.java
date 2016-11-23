8ffe37b2ab50ef25823bb129fe11fe634d46d104
student <student@virtualbox>
2016-11-23 10:34:39 +0100

    App.java corrected

--- SmashTheNexus/src/main/java/domain/App.java
+++ SmashTheNexus/src/main/java/domain/App.java
@@ -1,13 +1,29 @@
 package domain;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import dao.MatchRepository;
+import dao.TeamMemberRepository;
+import dao.TeamRepository;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String url = "jdbc:hsqldb:hsql://localhost/workdb";
+    	try {
+			Connection connection = DriverManager.getConnection(url);
+			MatchRepository matchrepo = new MatchRepository(connection);
+			TeamMemberRepository tmrepo = new TeamMemberRepository(connection);
+			TeamRepository accountRepo = new TeamRepository(connection);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    	
+        System.out.println( "Koniec" );
+        
     }
 }
